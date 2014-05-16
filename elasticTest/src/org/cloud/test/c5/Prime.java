3c81156974ebca565660bad6194d7248e84c3335
jcr34356532 <zzxstar@vip.qq.com>
2014-05-16 18:23:15 +0800

    test

--- elasticTest/src/org/cloud/test/c5/Prime.java
+++ elasticTest/src/org/cloud/test/c5/Prime.java
@@ -17,6 +17,19 @@ public class Prime {
 
 	}
 
+	public static boolean wanShu(int i) {
+		boolean flag = false;
+		int sum = 0;
+		for (int j = 1; j <= i / 2; j++) {
+			if (i % j == 0) {
+				sum += j;
+			}
+		}
+		if (sum == i)
+			flag = true;
+		return flag;
+	}
+
 	public static boolean everPrime(int i) {
 		boolean flag = true;
 		for (int j = i - 1; j > 1; j--) {
@@ -29,10 +42,20 @@ public class Prime {
 	}
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
 		for (int i = 14; i < 50; i++) {
 			if (isPrime(i))
 				list.add(i);
 		}
+
+		for (int i = 1; i < 1000; i++) {
+			if (wanShu(i))
+				list2.add(i);
+		}
+		for (Integer s : list2) {
+			System.out.println(s);
+		}
+
 	}
 
 }
